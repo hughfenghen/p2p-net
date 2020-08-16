a24e9aa4392e0b9a9216fc4b7fcc7297d0740776
hughfenghen <hughfenghen@gmail.com>
2020-08-16 20:47:26 +0800

    多点连接

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import Peer from 'peerjs';
+import { RemoteNode, MsgType } from './remote-node';
 
 const userId = Math.random().toString(36).slice(2)
-
+document.body.append(`userId: ${userId}`)
 async function discoverUser() {
   const userIds = await (await fetch('//192.168.1.2:9000/myapp/peerjs/peers')).json()
   return userIds.filter((id) => id !== userId)
@@ -11,30 +12,68 @@ async function main() {
   const peer = new Peer(userId, {
     host: '192.168.1.2',
     port: 9000,
-    path: '/myapp'
+    path: '/myapp',
+    config: {
+      iceServers: [
+        { urls: 'stun:ks-sh-live-p2p-01.chat.bilibili.com:3478' },
+        { urls: 'stun:stun.l.google.com:19302' },
+        // { urls: 'turn:homeo@turn.bistri.com:80', credential: 'homeo' },
+      ]
+    } /* Sample servers, please use appropriate ones */
   })
-  
+
+
   peer.on('connection', (conn) => {
-    conn.on('data', function (data) {
-      console.log('peer Received:', data);
-      document.body.innerText = data.toString();
+    console.log('------ connection:', conn)
+    conn.on('data', (data) => {
+      console.log('server Received:', data);
+
+      if (data.type === MsgType.Ping) {
+        conn.send({
+          ...data,
+          type: MsgType.Pong,
+        })
+      }
     });
   });
 
-  const otherIds = await discoverUser()
-  if (!otherIds.length) return
+  let rns: RemoteNode[] = []
+  setInterval(async () => {
+    const rnIds = rns.map(rn => rn.remoteId)
+    const discoverIds = (await discoverUser()).filter(id => !rnIds.includes(id))
+    if (!discoverIds.length) return
 
-  const conn = peer.connect(otherIds[0]);
-  conn.on('open', () => {
-    console.log('connection opened')
-    // Receive messages
-    conn.on('data', function (data) {
-      console.log('peer1 Received:', data);
-    });
+    console.log('------ discoverIds: ', discoverIds)
+    const newRns = discoverIds.filter(id => !rnIds.includes(id))
+      .map(id => new RemoteNode(peer, id))
+    newRns.forEach(nrn => {
+      const removeRn = () => {
+        rns = rns.filter(rn => rn.remoteId !== nrn.remoteId)
+        console.log('close conn; remain: ', rns)
+      }
+      nrn.conn.on('close', removeRn)
+      nrn.conn.on('error', removeRn)
 
-    // Send messages
-    conn.send('11111111111');
-  });
+    })
+
+    rns.push(...newRns)
+  }, 3000)
+
+  const div = document.createElement('div')
+  document.body.append(div)
+  setInterval(() => {
+    div.innerText = rns.map(rn => `remoteId: ${rn.remoteId}, delay: ${rn.delayTime}`).join('\n')
+  }, 1000)
 }
 
 main()
+
+function streamProducer(onData) {
+  const timerId = setInterval(() => {
+    onData(new Uint8Array(1024 * 100))
+  })
+
+  return () => {
+    clearInterval(timerId)
+  }
+}
---
+++ src/remote-node.ts
@@ -0,0 +1,84 @@
+import Peer from "peerjs";
+
+export enum MsgType {
+  Ping,
+  Pong,
+}
+
+export class RemoteNode {
+
+  conn: Peer.DataConnection
+
+  delayTime: number = Infinity
+
+  // status: 
+
+  private tryConnect = 0
+
+  private lastPingTs: number
+
+  private pingMsgs = new Set()
+
+  private pingMsgNo = 0
+
+  private pingTimer: number
+
+  constructor(
+    private selfPeer: Peer,
+    public remoteId: string,
+  ) {
+    console.log('==== new remote id: ', remoteId)
+
+    this.connect()
+  }
+
+  private connect() {
+    this.tryConnect += 1
+
+    this.conn = this.selfPeer.connect(this.remoteId);
+    console.log('remote connect: ', this.conn)
+    this.conn.on('open', () => {
+      console.log('connection opened')
+      this.tryConnect = -1
+      // Receive messages
+      this.conn.on('data', (data) => {
+        console.log('client Received:', data);
+        if (data.type === MsgType.Pong) {
+          this.onPong(data)
+        }
+      });
+      setInterval(() => {
+        this.ping()
+      }, 2000)
+    });
+
+    if (this.tryConnect === -1 || this.tryConnect > 3) return
+    
+    // setTimeout(() => {
+    //   this.connect()
+    // }, 3000)
+  }
+
+  private ping() {
+    const msg = {
+      type: MsgType.Ping,
+      ts: Date.now(),
+    }
+    // this.pingMsgs.add(msg)
+    this.conn.send(msg)
+    console.log('----- send:', msg)
+  }
+
+  private onPong(msg) {
+    this.delayTime = Date.now() - msg.ts
+    console.log(2222, this.delayTime)
+    // setTimeout(() => {
+    //   this.ping()
+    // }, 1000)
+  }
+
+  destory() {
+  
+  }
+
+}
